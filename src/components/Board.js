83914a8d109d2a8fb0154e6bb777aa78cff0cffb
i-nirbhay10 <nirbhayverma10@gmail.com>
2023-07-31 14:37:01 +0530

    created wining logic with restart button

--- src/components/Board.js
+++ src/components/Board.js
@@ -6,6 +6,29 @@ const Board = () => {
   const [state, setState] = useState(Array(9).fill(null));
   const [initial, Setinitial] = useState(true);
 
+  const checkwinner = () => {
+    const winner = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let logic of winner) {
+      const [a, b, c] = logic;
+
+      if (state[a] === state[b] && state[a] === state[c] && state[a] !== null) {
+        return state[a];
+      }
+    }
+  };
+
+  const win = checkwinner();
+
   const clicked = (index) => {
     const paststate = [...state];
     paststate[index] = initial ? "x" : "o";
@@ -21,6 +44,7 @@ const Board = () => {
     <>
       <div className="board_container">
         <h3> Welcome to tic-tac-toe game</h3>
+
         <div className="board_row">
           <Block onClick={() => clicked(0)} value={state[0]} />
           <Block onClick={() => clicked(1)} value={state[1]} />
@@ -36,6 +60,22 @@ const Board = () => {
           <Block onClick={() => clicked(7)} value={state[7]} />
           <Block onClick={() => clicked(8)} value={state[8]} />
         </div>
+        <h4>
+          {win ? (
+            <>
+              <h4> {`woo..! ${win} is win the match`}</h4>
+              <button
+                onClick={() => {
+                  setState([null]);
+                }}
+              >
+                restert
+              </button>
+            </>
+          ) : (
+            ""
+          )}
+        </h4>
       </div>
     </>
   );
